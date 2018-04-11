71be68c1d636850e0d113d322c3672497bf316fa
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-04-11 13:23:06 -0600

    Comit inicial. Upload del proyecto Ramon

---
+++ NegocioRepartidorApp/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace NegocioRepartidorApp
+{
+    public class Pedido
+    {
+
+
+        public int Id { get; set; }
+        public string NombreCliente { get; set; }
+        public int Descripcion { get; set; }
+        public string Ubicacion { get; set; }
+        public int Estado { get; set; }
+        public int Total { get; set; }
+        }
+    }
+
+//PedidoID, cliente, telefono, producto, estado,
